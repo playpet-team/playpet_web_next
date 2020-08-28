141e41bd17ab419aded68697c484c3c0cdbcd5df
baekkyoungjung <baekkyoungjung@gmail.com>
2020-08-29 02:08:42 +0900

    feat: emotion, material ui style SSR 적용 완료

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import NextApp from 'next/app'
 import { CacheProvider, Global } from '@emotion/core'
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 // Use only { cache } from 'emotion'. Don't use { css }.
 import { cache } from 'emotion'
@@ -7,11 +8,18 @@ import { cache } from 'emotion'
 import baseCSS from '../src/styles/baseCSS'
 
 export default class App extends NextApp {
+    componentDidMount() {
+        const jssStyles = document.querySelector('#jss-server-side');
+        if (jssStyles) {
+            jssStyles.parentNode.removeChild(jssStyles);
+        }
+    }
     render() {
         const { Component, pageProps } = this.props
         return (
             <CacheProvider value={cache}>
                 <Global styles={baseCSS} />
+                <CssBaseline />
                 <Component {...pageProps} />
             </CacheProvider>
         )
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,23 @@
-import Document, { Head, Main, NextScript } from 'next/document'
+import Document, { Head, Main, NextScript, DocumentContext } from 'next/document'
 import { extractCritical } from 'emotion-server'
+import { ServerStyleSheets } from '@material-ui/core/styles';
 
 export default class MyDocument extends Document {
-    static async getInitialProps(ctx) {
+    static async getInitialProps(ctx: DocumentContext) {
+        const sheets = new ServerStyleSheets();
+        const originalRenderPage = ctx.renderPage;
+        ctx.renderPage = () => originalRenderPage({
+            enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+        });
         const initialProps = await Document.getInitialProps(ctx)
         const styles = extractCritical(initialProps.html)
+
         return {
             ...initialProps,
             styles: (
                 <>
                     {initialProps.styles}
+                    {sheets.getStyleElement()}
                     <style
                         data-emotion-css={styles.ids.join(' ')}
                         dangerouslySetInnerHTML={{ __html: styles.css }}
