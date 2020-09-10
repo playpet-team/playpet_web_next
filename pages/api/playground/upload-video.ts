e2a566ea2bf57a437a2ad3e912eae36b7be1c8ed
baekkyoungjung <baekkyoungjung@gmail.com>
2020-09-10 17:15:26 +0900

    feat: 아씨 업로드 왜안되 촹나네

--- pages/api/playground/upload-video.ts
+++ pages/api/playground/upload-video.ts
@@ -8,26 +8,33 @@ const PROD_TEMPLATE = 'b871ce4999ec4fb895b26bba7e32d377'
 export default async function personHandler({ body: {
     cardId,
 }}: { body: { cardId: string; firename: string; }}, res) {
+    console.log("1")
     if (!cardId) {
         res.status(404)
     }
     try {
+        console.log("2")
         const response = await axios({
             method: 'GET',
             url: `https://storage.googleapis.com/playpet-5b432.appspot.com/playground/${cardId}`,
             responseType: 'stream'
         })
+        console.log("2")
         const transloadit = new Transloadit({
             authKey: process.env.TRANSLOADIT_KEY,
             authSecret: process.env.TRANSLOADIT_SECRET
         })
         const options = { params: { template_id: isProduction ? PROD_TEMPLATE : DEV_TEMPLATE }}
+        console.log("3")
         transloadit.addStream(cardId, response.data)
+        console.log("4")
         transloadit.createAssembly(options, (e, result) => {
+            console.log("5")
             if (e) {
                 Sentry.captureException(e)
                 res.status(404)
             }
+            console.log("6")
             res.status(200).json({
                 result
             })
