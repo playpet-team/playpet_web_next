562784ed3fdd231fd155b2eca16902422007f84f
baekkyoungjung <baekkyoungjung@gmail.com>
2020-11-09 19:21:52 +0900

    feat: 사료 밀어넣기 api test 제작

--- pages/api/test/index.ts
+++ pages/api/test/index.ts
@@ -1,6 +1,9 @@
+import { getCurrentTime } from './../../../src/utils/firebaseadmin';
+import { Collections } from './../../../src/utils/collections';
 import * as Sentry from '@sentry/node'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { apiSetup } from '..'
+import * as admin from 'firebase-admin'
 apiSetup()
 
 export default async function personHandler(req: NextApiRequest, res: NextApiResponse) {
@@ -8,7 +11,14 @@ export default async function personHandler(req: NextApiRequest, res: NextApiRes
         res.status(404)
     }
     try {
-        
+        // tet.forEach(async (te) => {
+        //     await admin.firestore().collection(Collections.Products).add({
+        //         ...te,
+        //         status: 'active',
+        //         createdAt: getCurrentTime(),
+        //         updatedAt: getCurrentTime(),
+        //     })
+        // })
         res.status(200).json({ status: 'success', })
     } catch (e) {
         Sentry.captureException(e)
--- src/utils/collections.ts
+++ src/utils/collections.ts
@@ -1,14 +1,16 @@
 export enum Collections {
     Users = 'users',
+    UserPets = 'pets',
     Terms = 'terms',
     AuthTokens = 'authTokens',
     Playground = 'playground',
     UserActions = 'userActions',
     PushSettings = 'pushSettings',
     Payments = 'payments',
+    CardInformations = 'cardInformations',
     Banners = 'banners',
     Products = 'products',
     Stores = 'stores',
     Notifications = 'notifications',
-    BusinessModel = 'businessModel',
+    ShippingDestination = 'shippingDestination',
 }
