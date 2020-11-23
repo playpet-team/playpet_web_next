1cc4e508cac800cae6e60d0a37e45be5dff1ac9e
baekkyoungjung <baekkyoungjung@gmail.com>
2020-11-23 14:04:32 +0900

    chore: 오류수정

--- pages/api/business-model/index.ts
+++ pages/api/business-model/index.ts
@@ -10,16 +10,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         res.status(404)
     }
     try {
-        const cards = await admin.firestore()
-            .collection(Collections.BusinessModel)
-            .get()
+        // const cards = await admin.firestore()
+        //     .collection(Collections.BusinessModel)
+        //     .get()
 
-        res.status(200).json(cards.docs.map((card: any) => {
-            return ({
-                ...card.data(),
-                id: card.id,
-            })
-        }))
+        // res.status(200).json(cards.docs.map((card: any) => {
+        //     return ({
+        //         ...card.data(),
+        //         id: card.id,
+        //     })
+        // }))
     } catch (e) {
         Sentry.captureException(e)
         res.status(404)
